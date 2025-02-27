5676d1a3829f6e8cd853085cadb10b06fba9c431
Marcel Menke <marcel.menke1981@gmail.com>
2025-02-27 09:59:25 +0100

    Profil Button
    - added profile button to layout header

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { ProfileDialogComponent } from '../profile-dialog/profile-dialog.component';
+import { UserProfileService } from '../../services/user-profile.service';
 
 @Component({
   selector: 'app-layout',
@@ -11,10 +14,27 @@ import { MatButtonModule } from '@angular/material/button';
     CommonModule,
     RouterModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule
   ],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent {
+  private dialog = inject(MatDialog);
+  private userProfileService = inject(UserProfileService);
+
+  openProfileDialog(): void {
+    const dialogRef = this.dialog.open(ProfileDialogComponent, {
+      width: '450px',
+      panelClass: ['custom-dialog', 'dark-theme'],
+      disableClose: false
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log('Dialog geschlossen mit:', result);
+      }
+    });
+  }
 }
