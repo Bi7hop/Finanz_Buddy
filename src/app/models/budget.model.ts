42d908fc80d446d2f0c4d1db15bf163efee765eb
Marcel Menke <marcel.menke1981@gmail.com>
2025-03-03 13:26:27 +0100

    updated models

--- src/app/models/budget.model.ts
+++ src/app/models/budget.model.ts
@@ -1,9 +1,10 @@
 export interface Budget {
-    id: number;
-    created_at: string;
-    user_id: string;
-    category: string;
-    amount: number;
-    period: string;
-    updated_at: string;
-  }
+  id: number;
+  created_at: string;
+  user_id: string;
+  category: string;       
+  category_icon?: string; 
+  amount: number;
+  period: string;
+  updated_at: string;
+}
--- src/app/models/transaction.model.ts
+++ src/app/models/transaction.model.ts
@@ -1,11 +1,13 @@
 export interface Transaction {
-    id: number;
-    created_at: string;
-    user_id: string;
-    amount: number;
-    date: string;
-    description: string;
-    category: string;
-    type: string;
-    updated_at: string;
-  }
+  id: number;
+  created_at: string;
+  user_id: string;
+  amount: number;
+  date: string;
+  description: string;
+  category: string;
+  type: string;
+  updated_at: string;
+  categoryName?: string;
+  isRecurring?: boolean;
+}
