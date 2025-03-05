a3a67a7e8f6eb2bb2278e3dc7a921e826b31852e
Marcel Menke <marcel.menke1981@gmail.com>
2025-03-05 12:12:38 +0100

    update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
+import { MatDialogModule } from '@angular/material/dialog';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimations(), 
-    provideHttpClient()
+    provideHttpClient(),
+    importProvidersFrom(MatDialogModule) 
   ]
 };
