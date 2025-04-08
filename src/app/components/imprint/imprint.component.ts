f2c35d3890d941b5f94c70b08889e37901983dc4
Marcel Menke <marcel.menke1981@gmail.com>
2025-04-08 10:26:25 +0200

    Added imprint component

---
+++ src/app/components/imprint/imprint.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-imprint',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './imprint.component.html',
+  styleUrls: ['./imprint.component.scss']
+})
+export class ImprintComponent { }
